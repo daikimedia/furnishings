bf91eb8840db9bd6ef5028e4372f4dcd6fba1428
daikimedia <lukesh@daikimedia.com>
2025-08-21 14:14:45 +0530

    dynamic sitemap integration

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,13 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: ['/api/', '/admin/', '/_next/'],
+            }
+        ],
         sitemap: 'https://www.furnishings.com.my/sitemap.xml',
     };
 }
