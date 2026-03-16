4041a01c4db72c999f76d7e71f3a31126fbe02f8
Munnaza <munnazashamim786@gmail.com>
2026-03-16 08:25:34 +0500

    browser cache enabled in next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,48 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cms.furnishings.daikimedia.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "cms.furnishings.daikimedia.com",
+        port: "",
+        pathname: "/**",
       },
     ],
+    minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=3600, stale-while-revalidate=86400",
+          },
+        ],
+      },
+      {
+        source: "/:all*(svg|jpg|jpeg|png|webp|gif|ico)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/shop/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, s-maxage=3600, stale-while-revalidate=86400",
+          },
+        ],
+      }
+    ];
   },
 };
 
